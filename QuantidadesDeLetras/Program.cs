3d7fa3dfcf53c2b9f3d8b2086db3b40f40a8c3cc
Henrique Greghi <henriquevsg@gmail.com>
2022-09-06 14:01:15 -0300

    add

--- QuantidadesDeLetras/Program.cs
+++ QuantidadesDeLetras/Program.cs
@@ -6,38 +6,48 @@ namespace QuantidadesDeLetras
     {
         static void Main(string[] args)
         {
-            
-            string palavras = " ";
-            int[] contador ;
-            string[] palavra;
+
+            string palavra = " ";
+            string[] palavras;
+            string maiorPalavra = "";
 
 
 
             Console.WriteLine("Entre com sua frase: ");
-            
-                while (palavras != "0")
+
+            while (true)
+            {
+
+                palavras = Console.ReadLine().Split(" ");
+                if (palavras[0] == "0") { break; }
+
+                for (int i = 0; i < palavras.Length; i++)
                 {
-
-                    palavras = Console.ReadLine();
-
-
-                    for (int i = 0; i < palavras.Length; i++)
+                    if (palavras[i].Length >= maiorPalavra.Length)
                     {
-                        
-                        if (Char.IsLetter(palavras[i]))
-                        {
+                        maiorPalavra = palavras[i];
+                    }
+                    if (i == palavra.Length - 1)
+                    {
 
-                        contador[i]++;
-                         
-                        }
+                        palavra += $"{palavras[i].Length}-";
 
                     }
-                    
+                    else
+                    {
+                        palavra += $"{palavras[i].Length}-";
+                    }
+
                 }
-                Console.WriteLine(contador);
+                Console.WriteLine(palavra);
+                
             }
+            
+
+
         }
     }
-
 }
+
+
 
